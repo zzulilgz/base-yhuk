a24b0060ba33b28abfaf2dbce58585211a31148c
liugaozhi <Gaozhi.Liu@b2bpas.com>
2019-05-18 10:38:28 +0800

    init

---
+++ base-zuul/src/main/java/com/yhuk/base/zuul/BaseZuulApplication.java
@@ -0,0 +1,13 @@
+package com.yhuk.base.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseZuulApplication.class, args);
+    }
+
+}
