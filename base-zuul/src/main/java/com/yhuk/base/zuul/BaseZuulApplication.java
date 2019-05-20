69bd0dde665325175f93c06af23bdef294354de4
liugaozhi <Gaozhi.Liu@b2bpas.com>
2019-05-20 18:36:54 +0800

    添加授权中心

--- base-zuul/src/main/java/com/yhuk/base/zuul/BaseZuulApplication.java
+++ base-zuul/src/main/java/com/yhuk/base/zuul/BaseZuulApplication.java
@@ -3,14 +3,18 @@ package com.yhuk.base.zuul;
 import com.yhuk.base.zuul.filters.LoggerFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-public class BaseZuulApplication {
+@EnableOAuth2Sso
+public class BaseZuulApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(BaseZuulApplication.class, args);
@@ -19,5 +23,18 @@ public class BaseZuulApplication {
     @Bean
     public LoggerFilter loggerFilter(){
         return new LoggerFilter();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+                 http
+                .authorizeRequests()
+                .antMatchers("/login","account")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable();
     }
 }
