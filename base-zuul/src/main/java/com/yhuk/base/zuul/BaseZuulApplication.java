a786cd04f2a44a4223bb6b7d6b2893ba527c431c
liugaozhi <Gaozhi.Liu@b2bpas.com>
2019-05-20 14:14:07 +0800

    init

--- base-zuul/src/main/java/com/yhuk/base/zuul/BaseZuulApplication.java
+++ base-zuul/src/main/java/com/yhuk/base/zuul/BaseZuulApplication.java
@@ -2,8 +2,12 @@ package com.yhuk.base.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class BaseZuulApplication {
 
     public static void main(String[] args) {
