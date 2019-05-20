e0f3c93fad135ef9d8774f04ac66cd14c11a536f
xiaolang <xiaolang@wangjubao.com>
2019-05-20 22:41:55 +0800

    添加auth

--- base-auth/src/main/java/com/yhuk/base/auth/BaseAuthApplication.java
+++ base-auth/src/main/java/com/yhuk/base/auth/BaseAuthApplication.java
@@ -2,12 +2,40 @@ package com.yhuk.base.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SpringBootApplication
-public class BaseAuthApplication {
+
+public class BaseAuthApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(BaseAuthApplication.class, args);
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER) //指定名称
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.inMemoryAuthentication()
+               .withUser("guest").password("guest")
+               .authorities("WRIGHT_READ")
+               .and()
+               .withUser("admin").password("admin").authorities("WRIGHT_READ","WRIGHT_WRITE");
+    }
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder(){
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 
 }
---
+++ base-auth/src/main/java/com/yhuk/base/auth/OAuthConfiguration.java
@@ -0,0 +1,55 @@
+package com.yhuk.base.auth;
+
+import jdk.nashorn.internal.parser.Token;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("zuul-server")
+                .secret("secret")
+                .scopes("WRIGHT","read").autoApprove(true)
+                .authorities("WRIGHT_READ","WRIGHT_WRITE")
+                .authorizedGrantTypes("implict","refresh_token","password","authorization_code");
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(jwtTokenStore())
+                .tokenEnhancer(jwtTokenConverter())
+                .authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    public TokenStore jwtTokenStore(){
+        return new JwtTokenStore(jwtTokenConverter());
+    }
+    @Bean
+    protected JwtAccessTokenConverter jwtTokenConverter(){
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("base-yhuk");
+        return jwtAccessTokenConverter;
+    }
+}
